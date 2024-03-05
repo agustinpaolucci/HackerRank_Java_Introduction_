cbcbce4e21d3cde97c9c522c7c81308e973c67b8
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2024-03-05 20:42:29 -0300

    Problem 01: Welcome to Java!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Problem 01: Welcome to Java!
+        Mostrar mensaje por consola */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
