a69cab1b3959c8264bfbf8196ad5fea599abdc0a
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2024-03-05 20:49:59 -0300

    Problem 02: Java Stdin and Stdout I

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*
         Problem 01: Welcome to Java!
-        Mostrar mensaje por consola */
+        Mostrar mensaje por consola
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
+        */
+
+        /*
+        Problem 02: Java Stdin and Stdout I
+        Ingresar valores de entrada con Scanner y mostrar los valores en salida con println()*/
+
+        Scanner scanner = new Scanner(System.in);
+        int miIntegral1 = scanner.nextInt();
+        int miIntegral2 = scanner.nextInt();
+        int miIntegral3 = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(miIntegral1);
+        System.out.println(miIntegral2);
+        System.out.println(miIntegral3);
     }
 }
