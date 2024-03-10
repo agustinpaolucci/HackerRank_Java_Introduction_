47b61f387a2b482238d98920c4be4195fba09463
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2024-03-10 20:32:33 -0300

    Problem 05: Java Output Formatting

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         Problem 01: Welcome to Java!
         Mostrar mensaje por consola
@@ -54,18 +66,35 @@ public class Main {
         Problem 04: Java Stdin and Stdout II
         Leer un int, un double y un string y sacarlos con System.out.println()
         HAY UN DETALLE IMPORTANTE CON SCANNER.NETXLINE();
-        */
+
 
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
         double d = scanner.nextDouble();
-        scanner.nextLine(); // IMPORTANTE!!!!!!!!!!!!!!!!!!
+        scanner.nextLine(); // IMPORTANTE PORQUE DESPUES DE UN NUMERO NEXT LINE NO LO PERMITE.
         String s = scanner.nextLine();
         scanner.close();
 
         System.out.println("String: " +s);
         System.out.println("Double: " +d);
         System.out.println("Int: " +i);
+        */
 
+        /*
+        Problem 05: Java Output Formatting
+        Uso del metodo printf con sus modificadotes y flags.
+        El método printf saca por consola un string con el formato que queremos.
+        */
+
+        System.out.println("================================");
+        Scanner scr =new Scanner(System.in);
+        for(int i = 0; i<3; i++){
+            String str01 = scr.next();
+            int num01 = scr.nextInt();
+            System.out.printf("%-15s", str01); // (s)string de 15 espacios justificado a la izquierda (-)
+            System.out.printf("%03d%n", num01); // (d) int de 3 espacios que si tiene menos ponga cero adelante y salto de lina
+        }
+        System.out.println("================================");
     }
 }
+
