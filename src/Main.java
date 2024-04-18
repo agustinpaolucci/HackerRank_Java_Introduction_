7315b99f93db43a01a499f26f7da0230eae0a159
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-17 21:14:04 -0300

    Update main

--- src/Main.java
+++ src/Main.java
@@ -245,6 +245,6 @@ public class Main {
         }
         // Lectura del IF:
         // si n que es int, es IGUAL, al convertir a S (que es string) a INT entonces... Good job
-
+        // Comment desde rama feature/nbcasa
     }
 }
